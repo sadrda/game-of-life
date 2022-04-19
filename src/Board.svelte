9e14c3da3e82df4bbd50cc9afe845b001c6615a1
Andreas Kofler <kofler_a@yahoo.de>
2022-04-19 09:17:09 +0200

    feat: add animation speed, board size

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
-  import { playing } from "./store";
+  import { animationSpeed, playing, boardSize } from "./store";
 
-  const BOARD_SIZE = 20;
+  let board = getRandomizedBoard();
+  let intervalId = -1;
 
-  let board = getEmptyBoard();
-  for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-      board[i][j] = Math.random() > 0.5;
-    }
-  }
-  function getEmptyBoard() {
-    let board = Array(BOARD_SIZE);
+  function getEmptyBoard(): boolean[][] {
+    let board = Array($boardSize);
     for (let i = 0; i < board.length; i++) {
-      board[i] = Array(BOARD_SIZE);
+      board[i] = Array($boardSize);
+    }
+    return board;
+  }
+
+  function getRandomizedBoard(): boolean[][] {
+    const board = getEmptyBoard();
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        board[i][j] = Math.random() > 0.5;
+      }
     }
     return board;
   }
@@ -62,11 +67,15 @@
     return nextBoard;
   }
 
-  setInterval(() => {
-    if ($playing) {
-      board = getNextGenBoard(board);
-    }
-  }, 1000);
+  boardSize.subscribe(() => (board = getRandomizedBoard()));
+  $: {
+    clearInterval(intervalId);
+    intervalId = window.setInterval(() => {
+      if ($playing) {
+        board = getNextGenBoard(board);
+      }
+    }, 1000 / $animationSpeed);
+  }
 </script>
 
 <div id="board-container">
@@ -104,6 +113,7 @@
     height: 100%;
     width: 100%;
     border: 1px solid grey;
+    cursor: pointer;
   }
   .cell.lit {
     background-color: rgb(21, 199, 21);
--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,5 @@
 import { writable } from "svelte/store";
 
 export const playing = writable(false);
+export const animationSpeed = writable(1);
+export const boardSize = writable(20);
